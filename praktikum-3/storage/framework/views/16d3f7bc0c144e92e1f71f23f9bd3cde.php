0d7984d5391207671112b56b1e7d85cec5e01ce1
ferdyvalentino123 <151491010+ferdyvalentino123@users.noreply.github.com>
2024-03-20 14:52:12 +0700

    Add files via upload

---
+++ praktikum-3/storage/framework/views/16d3f7bc0c144e92e1f71f23f9bd3cde.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Players</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+
+    <b>
+        <H3>Players Name List</H3>
+    </b>
+
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col">
+                <ul class="list-group list-group-horizontal">
+                    <li class="list-group-item">Starter</li>
+                    <li class="list-group-item">PG</li>
+                    <li class="list-group-item">Stephen Curry / 30</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <ul class="list-group list-group-horizontal-sm">
+                    <li class="list-group-item">Starter</li>
+                    <li class="list-group-item">SF</li>
+                    <li class="list-group-item">Gary Paython II</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <ul class="list-group list-group-horizontal-sm">
+                    <li class="list-group-item">Starter</li>
+                    <li class="list-group-item">SG</li>
+                    <li class="list-group-item">Klay Thompson</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <ul class="list-group list-group-horizontal-sm">
+                    <li class="list-group-item">Starter</li>
+                    <li class="list-group-item">PF</li>
+                    <li class="list-group-item">Jonathan Kuminga</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <ul class="list-group list-group-horizontal-sm">
+                    <li class="list-group-item">Starter</li>
+                    <li class="list-group-item"> CC </li>
+                    <li class="list-group-item">Draymond Green</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <ul class="list-group list-group-horizontal-xxl">
+                    <li class="list-group-item">Bench</li>
+                    <li class="list-group-item"> CC </li>
+                    <li class="list-group-item">Looney</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <ul class="list-group list-group-horizontal-xxl">
+                    <li class="list-group-item">Bench</li>
+                    <li class="list-group-item"> CC </li>
+                    <li class="list-group-item">Livingston</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <ul class="list-group list-group-horizontal-xxl">
+                    <li class="list-group-item">Bench</li>
+                    <li class="list-group-item">PF</li>
+                    <li class="list-group-item">Andrew Wiggins</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <ul class="list-group list-group-horizontal-xxl">
+                    <li class="list-group-item">Bench</li>
+                    <li class="list-group-item"> PG </li>
+                    <li class="list-group-item">CP 3</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <ul class="list-group list-group-horizontal-xxl">
+                    <li class="list-group-item">Bench</li>
+                    <li class="list-group-item"> PG </li>
+                    <li class="list-group-item">B. Podziemski</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <ul class="list-group list-group-horizontal-xxl">
+                    <li class="list-group-item">Bench</li>
+                    <li class="list-group-item"> SF </li>
+                    <li class="list-group-item">L. Quionines</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html><?php /**PATH C:\Users\ferdy valentino\praktikum-3\resources\views/player.blade.php ENDPATH**/ ?>
---
+++ praktikum-3/storage/framework/views/3ea8404b6f0afb7c9a1e1ec95c178bac.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Praktikum Soal No 2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        
+    <link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+
+    <!-- Navbar -->
+    <nav class="navbar fixed-top navbar-expand-lg bg-purple" data-bs-theme="dark">
+        <!-- Container -->
+        <div class="container py-2 px-4">
+            <!-- Navbar Brand -->
+            <a href="#" class="navbar-brand mb-0 h1">
+                <img class="img-fluid" src="../images/Logo-GSW.png" alt="logo" style="width: 40px;">
+            </a>
+            <!-- Navbar Toggler -->
+            <button type="button" class="navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- Offcanvas -->
+            <div class="offcanvas offcanvas-end bg-purple" id="offcanvasNavbar">
+                <!-- Offcanvas Body -->
+                <div class="offcanvas-body pt-0 px-4">
+                    <hr class="d-md-none text-white-50"> <!-- Horizontal Line -->
+                    <!-- Top Menu -->
+                    <ul class="navbar-nav flex-row flex-wrap">
+                        <li class="nav-item col-6 col-md-auto"><a href="#" class="nav-link active">Home</a></li>
+                        <li class="nav-item col-6 col-md-auto"><a href="player.html" class="nav-link">Players</a></li>
+                    </ul>
+                    <hr class="d-md-none text-white-50"> <!-- Horizontal Line -->
+                    <ul class="navbar-nav flex-row flex-wrap ms-md-auto">
+                        <li class="nav-item col-6 col-md-auto">
+                            <div class="vr d-none d-lg-flex h-100 mx-lg-2 text-white"></div>
+                            <hr class="d-lg-none my-2 text-white-50">
+                        </li>
+                        <li class="nav-item">
+
+                            <!-- Dropdown -->
+                            <div class="dropdown" data-bs-theme="light">
+                                <button type="button" class="btn nav-link text-white
+    dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <span class="d-lg-none">WARRIORS</span> Find More!
+                                </button>
+                                <ul class="dropdown-menu dropdown-menu-end">
+                                    <li>
+                                        <h6 class="dropdown-header">Jerseys
+                                        </h6>
+                                    </li>
+                                    <li><button class="dropdown-item active
+    bg-purple" type="button"><small>2015/2016<i class="bi-check"></i></small></button></li>
+                                    <li><button class="dropdown-item" type="button"><small>2017/2018</small></button>
+                                    </li>
+                                    <li><button class="dropdown-item" type="button"><small>2019/2020</small></button>
+                                    </li>
+                                    <li><button class="dropdown-item" type="button"><small>2021/2022</small></button>
+                                    </li>
+                                    <li><button class="dropdown-item" type="button"><small>2022/2023</small></button>
+                                    </li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li>
+                                        <h6 href="shop.html" class="dropdown-header">MERCH</h6>
+                                    </li>
+                                    <li><button class="dropdown-item" type="button"><small>bucket hat</small></button>
+                                    </li>
+                                    <li><button class="dropdown-item" type="button"><small>Towell</small></button></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="nav-item col-12 col-lg-auto">
+                            <div class="vr d-none d-lg-flex h-100 mx-lg-2
+    text-white"></div>
+                            <hr class="d-lg-none my-2 text-white-50">
+                        </li>
+                        <li class="nav-item">
+                    </ul>
+                </div>
+                </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div>
+        <div class="col-md-6 offset-md-3 text-center">
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" data-interval="50">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100"
+                            src="../images/Draymond-Green-Klay-Thompson-Stephen-Curry-1280.avif" alt="First slide"
+                            width="100%">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="../images/im-566198.jpeg" alt="Second slide" width="100%">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="../images/klayhit3.png" alt="Third slide" width="100%">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <h3>Welcome, Golden State Warriors Fams!</h3>
+    <p> Golden State Warriors adalah sebuah tim bola basket profesional Amerika Serikat yang bermarkas di San Francisco,
+        California.
+        Warriors berkompetisi di National Basketball Association (NBA), sebagai anggota Wilayah Barat, Divisi Pasifik.
+        Didirikan pada tahun 1946 di Philadelphia, Warriors pindah ke Wilayah Teluk San Francisco pada tahun 1962 dan
+        menggunakan nama
+        kota tersebut, sebelum mengubah nama geografisnya menjadi Golden State pada tahun 1971. Warriors memainkan
+        pertandingan di
+        markas mereka, Chase Center.</p>
+    <p> Warriors memenangkan gelar Basketball Association of America (BAA) perdana mereka pada tahun 1947, dan
+        memenangkan gelar kedua pada tahun 1956, dipimpin oleh trio Hall of Fame yang terdiri dari Paul Arizin, Tom
+        Gola, dan Neil Johnston. Namun, Warriors tidak pernah lagi mampu menyamai prestasi tersebut di Philadelphia.
+        Setelah Wariors menukar pemain bintang mereka—Wilt Chamberlain, mereka melalui periode perkembangan yang singkat
+        dan pindah ke San Francisco. Bersama para pemain bintang seperti Jamaal Wilkes dan Rick Barry, Warriors kembali
+        ke perebutan gelar, dan memenangkan gelar ketiga mereka pada tahun 1975 dalam pertandingan yang sebagian besar
+        dianggap sebagai salah satu hal paling mengejutkan dalam sejarah NBA.
+    </p>
+
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-sm">
+                <img src="../images/3ptshoot.jpeg"
+                    alt="" width="35%">
+            </div>
+            <div class="col-sm">
+                <img src="../images/B A Y.jpeg"
+                    alt="" width="50%">
+            </div>
+            <div class="col-sm">
+                <img src="../images/3ptshoot.jpeg"
+                    alt="" width="42%">
+            </div>
+        </div>
+    </div>
+
+
+    <p> Warriors kembali memasuki periode sulit pada tahun 1980-an, sebelum menjadi tim langganan babak playoff
+        menjelang tahun 1990-an dengan para pemain bintang seperti Tim Hardaway, Mitch Richmond, dan Chris Mullin.
+        Ketiganya dikenal dengan sebutan "Run TMC". Setelah gagal merebut gelar, Warriors memasuki fase konsolidasi pada
+        tahun 2000-an. Nasib Warriors berubah pada tahun 2010-an, mengantar era kesuksesan baru yang dipimpin oleh
+        Stephen Curry. Setelah merekrut para pemain All-Star seperti Klay Thompson dan Draymond Green, Warriors kembali
+        merebut gelar pada tahun 2015, sebelum memenangkan dua gelar lainnya pada tahun 2017 dan 2018 bersama mantan MVP
+        liga, Kevin Durant.</p>
+
+    <p> Warriors dijuluki Dubs sebagai kependekan dari huruf "W". Mereka juga memegang beberapa rekor NBA;
+        menjalani musim reguler terbaik sepanjang masa, meraih kemenangan terbanyak dalam satu musim (gabungan dari
+        musim reguler dan pascamusim), serta kemenangan pascamusim terbaik sepanjang masa. Dengan total tembakan
+        gabungan Curry dan Thompson, mereka berdua dianggap sebagai salah satu pasangan backcourt terbaik sepanjang
+        masa. Warriors telah mendapatkan tujuh gelar NBA, jumlah ketiga terbanyak dalam sejarah NBA. Menurut
+        Forbes, Warriors adalah waralaba olahraga dengan nilai tertinggi ketujuh di Amerika Serikat, dan kesepuluh di
+        dunia, dengan nilai perkiraan sekitar $3,1 miliar.</p>
+
+    <div class="card mb-3">
+        <img src="../images/SHOP logo.svg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Warriors Merch</h5>
+          <p class="card-text">Here is kind of merchs for fans of Golden State Warriors.</p>
+          <p class="card-text"><small class="text-body-secondary">Strength in Number</small></p>
+        </div>
+      </div>
+      <div class="card mb-3">
+        <img src="../images/Golden-State-Warriors-03.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Chase Center Court</h5>
+          <p class="card-text">This is the only place where warriors dinasty is begin.</p>
+          <p class="card-text"><small class="text-body-secondary">Strength in Number</small></p>
+        </div>
+      </div>
+   
+    <!-- Footer -->
+    <div id="footer" class="bg-light py-5">
+        <!-- Container -->
+        <div class="container py-5 px-4">
+            <div class="row">
+                <div class="col-lg-3 mb-5">
+                    <a href="#" class="logo text-decoration-none">
+                        <div class="d-flex">
+                            <img class="img-fluid" src="../images/Logo-GSW.png" alt="warriors Logo"
+                                style="width: 40px;">
+                            <div class="fs-5 ms-2 text-black">WARRIORS.COM</div>
+                        </div>
+                    </a>
+                    <div class="mt-2 text-muted">
+                        <small>Spread happiness with all the love in the world by
+                            the <a href="https://www.nba.com/warriors/" class="text-black">Warriors Team</a> with the help of <a href="https://www.nba.com/"
+                                class="text-black">NBA</a> as our
+                                contributors.</small>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-2 offset-lg-1 mb-5">
+                    <h5>Starter</h5>
+                    <ul class="list-unstyled">
+                        <li class="mb-2"><a href="#">Chef Curry</a></li>
+                        <li class="mb-2"><a href="#">Draymon Green</a></li>
+                        <li class="mb-2"><a href="#">Klay Thompson</a></li>
+                        <li class="mb-2"><a href="#">Andrew Wiggins</a></li>
+                        <li class="mb-2"><a href="#">CP 3</a></li>
+                        <li class="mb-2"><a href="#">Jonathan Kuminga</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-lg-2 mb-5">
+                    <h5>Bench</h5>
+                    <ul class="list-unstyled">
+                        <li class="mb-2"><a href="#">GP II</a></li>
+                        <li class="mb-2"><a href="#">Trace Jackon D</a></li>
+                        <li class="mb-2"><a href="#">Looney</a></li>
+                        <li class="mb-2"><a href="#">Brandon P</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-lg-2 mb-5">
+                    <h5>Champ Years</h5>
+                    <ul class="list-unstyled">
+                        <li class="mb-2"><a href="#">2015</a></li>
+                        <li class="mb-2"><a href="#">2016</a></li>
+                        <li class="mb-2"><a href="#">2018</a></li>
+                        <li class="mb-2"><a href="#">2022</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-lg-2 mb-5">
+                    <h5>Community</h5>
+                    <ul class="list-unstyled">
+                        <li class="mb-2"><a href="#">Bay Area</a></li>
+                        <li class="mb-2"><a href="#">SanFransisco Area</a></li>
+                        <li class="mb-2"><a href="#">National Basketball Association</a></li>
+                        <li class="mb-2"><a href="#">American Basketball</a></li>
+                        <li class="mb-2"><a href="#">World NBA</a></li>
+                    </ul>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html><?php /**PATH C:\Users\ferdy valentino\praktikum-3\resources\views/warrior.blade.php ENDPATH**/ ?>
